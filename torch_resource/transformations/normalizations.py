07faac7ae1c26a846605273a095d8999da76742f
Constantin Pape <c.pape@gmx.net>
2019-11-01 21:27:55 +0100

    Update requirements and normalization

--- torch_resource/transformations/normalizations.py
+++ torch_resource/transformations/normalizations.py
@@ -4,9 +4,13 @@ from .base import IndependentTransformation
 class NormalizeZeroMeanUnitVariance(IndependentTransformation):
     """ Sigmoid activation
     """
-    def __init__(self, apply_to, eps=1.e-6):
+    def __init__(self, apply_to, eps=1.e-6, mean=None, std=None):
         self.eps = eps
+        self.mean = mean
+        self.std = std
         super().__init__(apply_to=apply_to)
 
     def apply_transformation(self, tensor):
-        return (tensor - tensor.mean()) / (tensor.std() + self.eps)
+        mean = tensor.mean() if self.mean is None else mean
+        std = tensor.std() if self.std is None else std
+        return (tensor - mean) / (std + self.eps)
