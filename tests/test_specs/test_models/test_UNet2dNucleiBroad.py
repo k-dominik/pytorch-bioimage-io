9c7aceead62c196ac9f9200c468f309ce637bbd6
fynnbe <thefynnbe@gmail.com>
2020-01-17 14:03:12 +0100

    repair test_UNet2dNucleiBroad

--- tests/test_specs/test_models/test_UNet2dNucleiBroad.py
+++ tests/test_specs/test_models/test_UNet2dNucleiBroad.py
@@ -8,7 +8,7 @@ from pybio.spec import load_spec
 
 def test_UNet2dNucleiBroads():
     spec_path = (
-        Path(__file__).parent / "../../../specs/models/unet2d/nuclei_broad/UNet2dNucleiBroad.model.yaml"
+        Path(__file__).parent / "../../../specs/models/unet2d/nuclei_broad/UNet2DNucleiBroad.model.yaml"
     ).resolve()
     assert spec_path.exists(), spec_path
     loaded_spec = load_spec(spec_path.as_posix(), kwargs={})
