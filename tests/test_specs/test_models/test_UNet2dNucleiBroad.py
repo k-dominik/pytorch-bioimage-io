e173bb8edaa43e52f034a36d0d77a04a4b828165
fynnbe <thefynnbe@gmail.com>
2020-02-06 16:56:27 +0100

    repair test_UNet2dNucleiBroad

--- tests/test_specs/test_models/test_UNet2dNucleiBroad.py
+++ tests/test_specs/test_models/test_UNet2dNucleiBroad.py
@@ -6,12 +6,12 @@ import torch
 from pybio.spec import load_model, utils
 
 
-def test_UNet2dNucleiBroads():
+def test_UNet2dNucleiBroads(cache_path):
     spec_path = (
         Path(__file__).parent / "../../../specs/models/unet2d/nuclei_broad/UNet2DNucleiBroad.model.yaml"
     ).resolve()
     assert spec_path.exists(), spec_path
-    pybio_model = load_model(str(spec_path), kwargs={})
+    pybio_model = load_model(str(spec_path), cache_path=cache_path)
     with BytesIO() as f:
         model = utils.train(pybio_model, n_iterations=1, out_file=f)
         f.seek(0)
