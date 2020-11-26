ecb2426cd8de852e548a03856905a427e683b608
fynnbe <thefynnbe@gmail.com>
2020-11-26 10:39:26 +0100

    bump dependencies

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
-from io import open
-from os import path
+from pathlib import Path
 
 from setuptools import find_namespace_packages, setup
 
-here = path.abspath(path.dirname(__file__))
-
 # Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="pybio.torch",
-    version="0.1a",
+    version="0.3a",
     description="Common torch based components for bioimage zoo",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,14 +16,13 @@ setup(
     classifiers=[  # Optional
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     packages=find_namespace_packages(exclude=["tests"]),  # Required
     install_requires=[
-        "torch>=1.1",
+        "torch>=1.4",
         "numpy>=1.17",
-        "imageio>=2.5",
         "pybio.core @ git+http://github.com/bioimage-io/python-bioimage-io#egg=pybio.core",
     ],
     extras_require={"test": ["tox", "pytest"]},
