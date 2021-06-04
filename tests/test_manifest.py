a2c14f266b35cdf95bef9bd960936a8112fb4695
fynnbe <thefynnbe@gmail.com>
2021-06-04 17:33:55 +0200

    fix test_validate_manifest with autoconvert

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -1,5 +1,9 @@
-from bioimageio.spec.__main__ import verify_bioimageio_manifest
+from ruamel.yaml import YAML
+
+from bioimageio.spec.__main__ import verify_bioimageio_manifest_data
+
+yaml = YAML(typ="safe")
 
 
 def test_validate_manifest(manifest_path):
-    verify_bioimageio_manifest(manifest_path)
+    verify_bioimageio_manifest_data(yaml.load(manifest_path), auto_convert=True)
