2312f4815d31f7d26629a9144a8716492f7af265
fynnbe <thefynnbe@gmail.com>
2021-06-04 11:41:37 +0200

    add minfest path as fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,8 @@ import pytest
 @pytest.fixture
 def cache_path(tmp_path):
     return Path(os.getenv("PYBIO_CACHE_PATH", tmp_path))
+
+
+@pytest.fixture
+def manifest_path():
+    return Path(__file__).parent.parent / "manifest.yaml"
--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -1,27 +1,28 @@
 from pathlib import Path
 
-import pytest
 import yaml
 
-from bioimageio.spec import utils, load_and_resolve_spec
-
-MANIFEST_PATH = Path(__file__).parent.parent / "manifest.yaml"
+from bioimageio.spec import load_and_resolve_spec, utils
+from bioimageio.spec.__main__ import verify_bioimageio_manifest
 
 
 def pytest_generate_tests(metafunc):
+    manifest_path = Path(__file__).parent.parent / "manifest.yaml"
     if "category" in metafunc.fixturenames and "spec_path" in metafunc.fixturenames:
-        with MANIFEST_PATH.open() as f:
+        with manifest_path.open() as f:
             manifest = yaml.safe_load(f)
 
         categories_and_spec_paths = [
-            (category, spec_path) for category, spec_paths in manifest.items() for spec_path in spec_paths
+            (category, manifest_path.parent / spec_path) for category, spec_paths in manifest.items() for spec_path in spec_paths
         ]
         metafunc.parametrize("category,spec_path", categories_and_spec_paths)
 
 
-def test_load_specs_from_manifest(category, spec_path):
+def test_validate_manifest(manifest_path):
+    verify_bioimageio_manifest(manifest_path)
 
-    spec_path = MANIFEST_PATH.parent / spec_path
+
+def test_load_specs_from_manifest(category, spec_path):
     assert spec_path.exists()
 
     loaded_spec = load_and_resolve_spec(spec_path)
